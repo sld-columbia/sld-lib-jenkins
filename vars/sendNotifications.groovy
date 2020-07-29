a335bdaed7e71e0093c210bc33a0dc839657a482
Davide Giri <davide_giri@cs.columbia.edu>
2020-07-29 05:26:46 -0400

    sendNotifications: modify Slack message format

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -8,34 +8,35 @@ def call(String buildStatus = 'STARTED') {
     buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
     // Default values
-    def colorName = 'RED'
-    def colorCode = '#FF0000'
-    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}] (<${env.RUN_DISPLAY_URL}|Open>) (<${env.RUN_CHANGES_DISPLAY_URL}|  Changes>)'"
-    def title = "${env.JOB_NAME} Build: ${env.BUILD_NUMBER}"
-    def title_link = "${env.RUN_DISPLAY_URL}"
+    def colorCode = 'good'
     def branchName = "${env.BRANCH_NAME}"
-    def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
+    // def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
     def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an'").trim()
-    def message = sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
-    def summary = "${subject} (${env.BUILD_URL})"
+    // def message = sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
+    // def summary = "${subject} (${env.BUILD_URL})"
     def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
     // Override default values based on build status
     if (buildStatus == 'STARTED') {
-	color = 'YELLOW'
-	colorCode = '#FFFF00'
-    } else if (buildStatus == 'SUCCESSFUL') {
-	color = 'GREEN'
 	colorCode = 'good'
+	postStatus = 'Started'
+	
+    } else if (buildStatus == 'SUCCESSFUL') {
+	colorCode = 'good'
+	postStatus = 'Success'
+
     } else if (buildStatus == 'UNSTABLE') {
-	color = 'YELLOW'
 	colorCode = 'warning'
+	postStatus = 'Unstable'
+
     } else {
-	color = 'RED'
 	colorCode = 'danger'
+	postStatus = 'Failure'
     }
 
+    def subject = "${env.JOB_NAME} - #${env.BUILD_NUMBER} ${postStatus} (<${env.RUN_DISPLAY_URL}|Open>) (<${env.RUN_CHANGES_DISPLAY_URL}|  Changes>). Branch: ${branchName}"
+    
     // Send notifications
     slackSend (color: colorCode, message: subject)
 
